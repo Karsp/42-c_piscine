5f49a2d83abb2adaae8c7f410c538525c149b116
David Aviles Leiva <daviles-@c1r13s2.42madrid.com>
2023-04-24 16:49:28 +0200

    working but lot of leaks

--- cursus/gnl/get_next_line.c
+++ cursus/gnl/get_next_line.c
@@ -6,23 +6,30 @@
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 22:06:04 by daviles-          #+#    #+#             */
-/*   Updated: 2023/04/17 22:12:10 by daviles-         ###   ########.fr       */
+/*   Updated: 2023/04/24 16:46:59 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"get_next_line.h"
 
+void	ft_free(char *ptr, char *ptr2)
+{
+	free(ptr);
+	ptr = NULL;
+	free(ptr2);
+	ptr2 = NULL;
+}
+
 void	ft_savewaste(char *buf, char **waste)
 {
 	int			from;
-	char		*old;
 
 	if (ft_strposchr(buf, '\n') == -1)
 		from = 0;
 	else
 		from = ft_strposchr(buf, '\n') + 1;
-	old = *waste;
+	free(*waste);
+	*waste = NULL;
 	*waste = ft_substr(buf, from, ft_strlen(buf));
-	free(old);
 }
 
 char	*ft_saveline(void *buf)
@@ -40,27 +47,35 @@ char	*ft_saveline(void *buf)
 
 char	*ft_read(char **waste, void *buf, int fd)
 {
+	int		str;
+	char	*join;
+
+	str = read(fd, buf, BUFFER_SIZE);
 	while (ft_strposchr((char *)buf, '\n') == -1)
 	{
-		ft_savewaste((char *)buf, &*waste);
-		if (read(fd, buf, BUFFER_SIZE) == 0)
+		if (str <= 0)
 		{
-			ft_savewaste((char *)buf, &*waste);
+			if (str == 0)
+				ft_savewaste((char *)buf, &*waste);
 			return (NULL);
 		}
-		if (read(fd, buf, BUFFER_SIZE) == -1)
-			return (NULL);
+		join = ft_strjoin(*waste, buf);
+		ft_savewaste(join, &*waste);
+		free(join);
+		str = read(fd, buf, BUFFER_SIZE);
 	}
+	// devolver waste, meter read en bucle
 	return (buf);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*buf;
+	void		*buf;
 	char		*line;
+	char		*join;
 	static char	*waste;
 
-	if (fd <= 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	buf = (char *) ft_calloc (BUFFER_SIZE + 1, 1);
 	if (waste == NULL)
@@ -73,37 +88,33 @@ char	*get_next_line(int fd)
 	}
 	if (ft_read(&waste, buf, fd) == NULL)
 	{
-		free(waste);
-		free(buf);
+		ft_free(waste, buf);
 		return (NULL);
 	}
-	line = ft_saveline(ft_strjoin(waste, (char *) buf));
+	join = ft_strjoin(waste, (char *) buf);
+	line = ft_saveline(join);
 	ft_savewaste((char *)buf, &waste);
+	ft_free(join, buf);
 	return (line);
 }
-/* 
+
 int	main(void)
 {
 	int		fd;
 	char	*line = "";
+	int		i;
 
+	i = 1;
 	fd = open("test.txt", O_RDONLY);
-//	fd = 8;
-
-	printf("Result 1: %s\n", get_next_line(fd));
-	printf("Result 2: %s\n", get_next_line(fd));
-	printf("Result 3: %s\n", get_next_line(fd));
-	printf("Result 4: %s\n", get_next_line(fd));
-	printf("Result 5: %s\n", get_next_line(fd));
-	printf("Result 6: %s\n", get_next_line(fd));
 	while (line)
 	{
 		line = get_next_line(fd);
-		printf("%s", line);
+		printf("Result %d: %s\n", i, line);
 		free(line);
+		i++;
 	}
-
     close(fd);
+//	system("leaks a.out");
 	return (0);
 }
-*/
+
